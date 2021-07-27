258d721e1b4965c2ca1f88594de02463c203977f
Matheus Gomes <gomesprogramacao@gmail.com>
2021-07-27 12:24:04 -0300

    feat: style Global

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
+// styles
+import GlobalStyles from './styles/GlobalStyles';
+
 function App() {
-  return <h1>Ola pais</h1>;
+  return (
+    <>
+      <h1>Olá mundo</h1>
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default App;
