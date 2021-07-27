cab7ad3683801afb8611b3603f73a8d1c16cae5d
Matheus Gomes <gomesprogramacao@gmail.com>
2021-07-27 12:42:30 -0300

    feat: create component Layout and e style

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+// components
+import Main from './Main';
+
+// styles
+import { Container, Wrapper } from './styles';
+
+const Layout: React.FC = () => {
+  return (
+    <Container>
+      <Wrapper>
+        {/* <MenuBar /> */}
+        <Main />
+        {/* <Sidebar /> */}
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Layout;
