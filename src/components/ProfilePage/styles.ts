c4e36275c822ea3a092b2514cb0ce6a1d41c257d
Matheus Gomes <gomesprogramacao@gmail.com>
2021-07-30 13:02:43 -0300

    feat: style of Container, Banner and icons LocationIcons ,CakeIcon

--- src/components/ProfilePage/styles.ts
+++ src/components/ProfilePage/styles.ts
@@ -1,3 +1,41 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { LocationOn, Cake } from '../../styles/Icons';
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
 
-export const Container = styled.div``;
+  max-height: 100%;
+  overflow: auto;
+
+  scrollbar-width: none; /* firefox */
+  ::-webkit-scrollbar {
+    /* chrome */
+    display: none;
+  }
+`;
+export const Banner = styled.div`
+  flex-shrink: 0;
+
+  width: 100%;
+  height: min(33vw, 199px);
+
+  background-color: var(--twitter);
+  position: relative;
+`;
+export const Avatar = styled.div``;
+export const ProfileData = styled.div``;
+export const Followage = styled.div``;
+
+const icoCSS = css`
+  width: 20px;
+  height: 20px;
+
+  color: var(--gray);
+`;
+
+export const LocationIcons = styled(LocationOn)`
+  ${icoCSS}
+`;
+export const CakeIcon = styled(Cake)`
+  ${icoCSS}
+`;
